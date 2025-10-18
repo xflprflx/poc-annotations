c04d94690a7a4d5d18fa2c7076b2252c450ebc1e
xflprflx <x.filipe.machado.x@gmail.com>
2025-10-18 13:51:35 -0300

    projeto criado

---
+++ src/main/java/com/xflprflx/poc_annotations/PocAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.xflprflx.poc_annotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocAnnotationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xflprflx/poc_annotations/PocAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xflprflx.poc_annotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocAnnotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
